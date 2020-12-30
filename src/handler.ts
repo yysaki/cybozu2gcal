651cfa89a6749fd1ebc49b951d9e34c4b50d3903
yysaki <mail@yysaki.com>
2020-12-30 09:16:11 +0900

    Initialize chrome-aws-lambda

--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,33 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import chromium from 'chrome-aws-lambda';
+import { Browser } from 'puppeteer';
 
-export const cybozu2gcal: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v0.1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
-  };
+export const cybozu2gcal: APIGatewayProxyHandler = async () => {
+  let browser: Browser | null = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto('https://www.google.com');
+
+    const result = await page.title();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: result }),
+    };
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
 }
