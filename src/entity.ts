54e99af085729b6b2630fc0e7f5c6a1eb498e217
yysaki <mail@yysaki.com>
2020-12-31 17:20:22 +0900

    Move buildDateTime to entity

--- src/entity.ts
+++ src/entity.ts
@@ -1,5 +1,23 @@
 import 'source-map-support/register';
 
+interface BuildDateTimeParam {
+  year: string,
+  month: string,
+  day: string,
+  hour: string,
+  minute: string
+}
+
+export const buildDateTime = ({ year, month, day, ...rest }: BuildDateTimeParam) => {
+  const pad = (num: string) => ('00' + num).slice(-2);
+
+  // 24:00 を 23:59 に読み替え
+  const hour = rest.hour === '24' ? '23' : rest.hour;
+  const minute = rest.hour === '24' ? '59' : rest.minute;
+
+  return `${year}-${pad(month)}-${pad(day)}T${pad(hour)}:${pad(minute)}:00+09:00`; // Asia/Tokyo
+}
+
 export interface Event {
   title: string;
   eid: string;
@@ -7,3 +25,4 @@ export interface Event {
   startedAt: string;
   endedAt: string;
 };
+
--- src/fetchEventsFromCybozuUsecase.ts
+++ src/fetchEventsFromCybozuUsecase.ts
@@ -1,30 +1,12 @@
 import 'source-map-support/register';
 import { Page } from 'puppeteer';
 
-import { Event } from './entity';
-
-interface BuildDateParam {
-  year: string,
-  month: string,
-  day: string,
-  hour: string,
-  minute: string
-}
+import { buildDateTime, Event } from './entity';
 
 interface QueryResult {
   title: string,
   href: string,
   eventTime?: string,
-}
-
-const buildDate = ({ year, month, day, ...rest }: BuildDateParam) => {
-  const pad = (num: string) => ('00' + num).slice(-2);
-
-  // 24:00 を 23:59 に読み替え
-  const hour = rest.hour === '24' ? '23' : rest.hour;
-  const minute = rest.hour === '24' ? '59' : rest.minute;
-
-  return `${year}-${pad(month)}-${pad(day)}T${pad(hour)}:${pad(minute)}:00+09:00`; // Asia/Tokyo
 }
 
 const parseResult = ({ title, href, eventTime }: QueryResult): Event | undefined => {
@@ -52,8 +34,8 @@ const parseResult = ({ title, href, eventTime }: QueryResult): Event | undefined
     }
   }
 
-  const startedAt = buildDate({ ...startedDate, ...startedTime });
-  const endedAt = buildDate({ ...endedDate, ...endedTime });
+  const startedAt = buildDateTime({ ...startedDate, ...startedTime });
+  const endedAt = buildDateTime({ ...endedDate, ...endedTime });
 
   return { title , eid, startedAt, endedAt };
 }
