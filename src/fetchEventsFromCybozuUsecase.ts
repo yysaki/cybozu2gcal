8b6fcaf431fd3227c13193077aa897a692f0fbfb
yysaki <mail@yysaki.com>
2020-12-31 15:41:50 +0900

    Extract parseResult

--- src/fetchEventsFromCybozuUsecase.ts
+++ src/fetchEventsFromCybozuUsecase.ts
@@ -11,61 +11,69 @@ interface BuildDateParam {
   minute: string
 }
 
-const fetchEvents = async (schedulePage: Page): Promise<Event[]> => {
-  return await schedulePage.evaluate(() => {
-    const result: Event[] = [];
-
-    document.querySelectorAll('.eventLink').forEach(element => {
-      const event: HTMLAnchorElement | null = element.querySelector('a.event');
-      if (!event) return;
-
-      const regexp = /\Date=da\.([0-9]+)\.([0-9]+)\.([0-9]+)\&BDate=da\.([0-9]+)\.([0-9]+)\.([0-9]+)\&sEID=([0-9]+)/
-      const match = event.href.match(regexp);
-      if (!match) return;
-
-      const eid = match[7];
-
-      const startedDate = { year: match[1], month: match[2], day: match[3] };
-      const endedDate = { year: match[4], month: match[5], day: match[6] };
-      let startedTime = { hour: '0', minute: '0' };
-      let endedTime = { hour: '23', minute: '59' };
-
-      const eventTime: HTMLSpanElement | null = element.querySelector('span.eventDateTime');
-      if (eventTime?.innerText) {
-        const timeSpanMatch = eventTime.innerText.match(/([0-9]+):([0-9]+)-([0-9]+):([0-9]+)/);
-        const timeMatch = eventTime.innerText.match(/([0-9]+):([0-9]+)/);
-
-        if (timeSpanMatch) {
-          startedTime = { hour: timeSpanMatch[1], minute: timeSpanMatch[2] };
-          startedTime = { hour: timeSpanMatch[3], minute: timeSpanMatch[4] };
-        } else if (timeMatch) {
-          startedTime = { hour: timeMatch[1], minute: timeMatch[2] };
-          startedTime = { hour: timeMatch[1], minute: timeMatch[2] };
-        }
-      }
-
-      const buildDate = (param: BuildDateParam) => {
-        const pad = (num: string) => ('00' + num).slice(-2);
-
-        // 24:00 を 23:59 に読み替え
-        const hour = param.hour === '24' ? '23' : param.hour;
-        const minute = param.hour === '24' ? '59' : param.minute;
-
-        return `${param.year}-${pad(param.month)}-${pad(param.day)}T${pad(hour)}:${pad(minute)}:00+09:00`; // Asia/Tokyo
-      }
-
-      const startedAt = buildDate({ ...startedDate, ...startedTime });
-      const endedAt = buildDate({ ...endedDate, ...endedTime });
-
-      result.push({ title: event.title, eid, startedAt, endedAt });
-    });
-
-    return result;
-  });
+interface QueryResult {
+  title: string,
+  href: string,
+  eventTime?: string,
 }
 
-export const fetchEventsFromCybozuUsecase = (page: Page) => {
+const buildDate = ({ year, month, day, ...rest }: BuildDateParam) => {
+  const pad = (num: string) => ('00' + num).slice(-2);
+
+  // 24:00 を 23:59 に読み替え
+  const hour = rest.hour === '24' ? '23' : rest.hour;
+  const minute = rest.hour === '24' ? '59' : rest.minute;
+
+  return `${year}-${pad(month)}-${pad(day)}T${pad(hour)}:${pad(minute)}:00+09:00`; // Asia/Tokyo
+}
+
+const parseResult = ({ title, href, eventTime }: QueryResult): Event | undefined => {
+  const regexp = /\Date=da\.([0-9]+)\.([0-9]+)\.([0-9]+)\&BDate=da\.([0-9]+)\.([0-9]+)\.([0-9]+)\&sEID=([0-9]+)/
+  const match = href.match(regexp);
+  if (!match) return;
+
+  const eid = match[7];
+
+  const startedDate = { year: match[1], month: match[2], day: match[3] };
+  const endedDate = { year: match[4], month: match[5], day: match[6] };
+  let startedTime = { hour: '0', minute: '0' };
+  let endedTime = { hour: '23', minute: '59' };
+
+  if (eventTime) {
+    const timeSpanMatch = eventTime.match(/([0-9]+):([0-9]+)-([0-9]+):([0-9]+)/);
+    const timeMatch = eventTime.match(/([0-9]+):([0-9]+)/);
+
+    if (timeSpanMatch) {
+      startedTime = { hour: timeSpanMatch[1], minute: timeSpanMatch[2] };
+      startedTime = { hour: timeSpanMatch[3], minute: timeSpanMatch[4] };
+    } else if (timeMatch) {
+      startedTime = { hour: timeMatch[1], minute: timeMatch[2] };
+      startedTime = { hour: timeMatch[1], minute: timeMatch[2] };
+    }
+  }
+
+  const startedAt = buildDate({ ...startedDate, ...startedTime });
+  const endedAt = buildDate({ ...endedDate, ...endedTime });
+
+  return { title , eid, startedAt, endedAt };
+}
+
+export const fetchEventsFromCybozuUsecase = (schedulePage: Page) => {
   return async () => {
-    return await fetchEvents(page);
+    const evaluated = await schedulePage.evaluate(() => {
+      const result: QueryResult[] = [];
+
+      document.querySelectorAll('.eventLink').forEach(element => {
+        const event: HTMLAnchorElement | null = element.querySelector('a.event');
+        if (!event) return;
+
+        const eventTime: HTMLSpanElement | null = element.querySelector('span.eventDateTime');
+        result.push({ title: event.title, href: event.href, eventTime: eventTime?.innerText });
+      });
+
+      return result;
+    });
+
+    return evaluated.map(parseResult).filter<Event>((e): e is Event => !!e);
   }
 }
