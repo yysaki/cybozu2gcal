51758241fbbcb5bfefbe41dfec5ef3743b866da5
yysaki <mail@yysaki.com>
2021-01-11 14:37:52 +0900

    Add @slack/webhook

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,7 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'; // eslint-disable-line import/no-unresolved
 import { evaluateWebPage, createCybozuRepository } from './api/cybozu';
 import { calendarDriver, createGoogleCalendarRepository } from './api/googleCalendar';
+import { slackNotify } from './lib';
 import { syncUsecaseInteractor } from './usecase/cybozu2gcal';
 
 export const cybozu2gcal: APIGatewayProxyHandler = async () => {
@@ -10,11 +11,14 @@ export const cybozu2gcal: APIGatewayProxyHandler = async () => {
 
     const sync = syncUsecaseInteractor(cybozuRepository, googleCalendarRepository);
     const { inserted, deleted } = await sync();
+    const body = `inserted: ${inserted.length}, deleted: ${deleted.length}`;
 
-    return { statusCode: 200, body: `inserted: ${inserted.length}, deleted: ${deleted.length}` };
+    await slackNotify({ status: 'good', message: body });
+    return { statusCode: 200, body };
   } catch (error) {
     console.error(error);
 
+    await slackNotify({ status: 'warning', message: error?.message || 'Unknown error was thrown.' });
     return { statusCode: 500, body: JSON.stringify(error) };
   }
 };
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,2 @@
 export * from './dayjs';
+export * from './slackNotify';
