aa2899b1d194bbb34651824d2bdc34e45bed39df
yysaki <mail@yysaki.com>
2021-03-27 12:02:51 +0900

    Fix DateTime not to care TZ

--- src/entity/dateTime.ts
+++ src/entity/dateTime.ts
@@ -9,14 +9,16 @@ dayjs.extend(timezone);
 
 type Dayjs = dayjs.Dayjs;
 
+const isISOString = (timestamp: string) => /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\+\d{2}:\d{2}$/.test(timestamp);
+
 export class DateTime {
   #dateTime: Dayjs;
 
-  constructor(date: dayjs.ConfigType) {
-    this.#dateTime = dayjs(date).tz(TIMEZONE);
+  constructor(timestamp: string) {
+    this.#dateTime = isISOString(timestamp) ? dayjs(timestamp).tz(TIMEZONE) : dayjs.tz(timestamp, TIMEZONE);
   }
 
-  add1Day = (): DateTime => new DateTime(this.#dateTime.add(1, 'day'));
+  add1Day = (): DateTime => new DateTime(this.#dateTime.add(1, 'day').format());
   format = (template?: string | undefined): string => this.#dateTime.format(template);
   toJSON = (): string => this.format();
   unix = (): number => this.#dateTime.unix();
